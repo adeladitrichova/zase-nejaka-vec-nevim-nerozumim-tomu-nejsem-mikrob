c67aa4a12714fd0de749841c73663d2fd74e2cff
Adéla Ditrichová <91247571+adeladitrichova@users.noreply.github.com>
2022-04-21 00:22:31 +0200

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,67 @@
+#klient
+
+radio.set_group(28)
+
+answer = 0
+alphabet = ['A', 'B', 'C', 'D', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 
+'M', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+options = alphabet.count(alphabet)
+
+def on_button_pressed_a():
+    global answer
+    if answer == 0:
+        answer = options - 1
+    else:
+        answer = (answer-1)%options
+        basic.show_string(alphabet[answer])
+    basic.clear_screen()
+
+def on_button_pressed_b():
+    global answer
+    if answer == 0:
+        answer = options + 1
+    else:
+        answer = (answer+1)%options
+        basic.show_string(alphabet[answer])
+    basic.clear_screen()
+
+
+def on_logo_is_pressed():
+    radio.send_string(alphabet[answer])
+
+input.on_button_pressed(Button.A, on_button_pressed_a)
+input.on_button_pressed(Button.B, on_button_pressed_b)
+input.on_logo_up(on_logo_is_pressed)
+
+
+
+
+
+
+
+
+
+
+
+#server
+
+radio.set_group(28)
+
+
+#klient
+#kdykoliv muze odeslat hlas
+#vzdy se zapisuje posledni odpoved a zapisuje se jen jedna odpoved
+
+#server
+#prepina stav >prijima< x >neprijima<
+#zapisuje se vzdy jedna odpoved od kazdeho
+#zobrazi, kolik lidi dalo A, B, C
+#vynuluje svoji "pamet" pak proste yk
+
+
+#A 65
+#B 66
+#C 67
+#D 68
+#odpovedi v listu, aby byl neomezemy pocet odpovedi jakoze ykykykykykykykykyk
+#provest listu v listu
---
+++ main.ts
@@ -0,0 +1,32 @@
+// klient
+radio.setGroup(28)
+let answer = 0
+let alphabet = ["A", "B", "C", "D", "F", "G", "H", "I", "J", "K", "L", "M", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+let options = _py.py_array_count(alphabet, alphabet)
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
+    if (answer == 0) {
+        answer = options - 1
+    } else {
+        answer = (answer - 1) % options
+        basic.showString(alphabet[answer])
+    }
+    
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    
+    if (answer == 0) {
+        answer = options + 1
+    } else {
+        answer = (answer + 1) % options
+        basic.showString(alphabet[answer])
+    }
+    
+    basic.clearScreen()
+})
+input.onLogoUp(function on_logo_is_pressed() {
+    radio.sendString(alphabet[answer])
+})
+// server
+radio.setGroup(28)
